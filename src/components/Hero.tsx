e06f34e799aff5e3941bf12e7b1a3d3fe60bbd0c
shibiraj <124015659+shibi22@users.noreply.github.com>
2025-01-29 10:58:43 +0530

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -42,7 +42,7 @@ const Hero = () => {
       <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
         <img
           className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
-          src='/images/heroSectionImage.jpg'
+          src='./images/heroSectionImage.jpg'
           alt="Modern workspace with laptop and coffee"
         />
       </div>
